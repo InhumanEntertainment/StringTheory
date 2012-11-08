bb3f2975df8b630a3dc5b69e7f46e0ab521e13ac
Erik Larsson <ecl3d@hotmail.com>
2012-11-08 14:51:42 +0700

    Disabled Storekit

--- Unity/Assets/Scripts/Store.cs
+++ Unity/Assets/Scripts/Store.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -50,4 +50,4 @@ public class StringTheoryProduct
         }
     }
 }
-
+*/
