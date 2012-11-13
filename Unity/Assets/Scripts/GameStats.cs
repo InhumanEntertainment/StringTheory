2faa6b64c1902f3a357d0cb85bcb0e46ea450981
Erik Larsson <ecl3d@hotmail.com>
2012-11-13 15:23:54 +0700

    Removed Dimension Prototype | Rated All Levels | Updated Packs | Added Time HUD

--- Unity/Assets/Scripts/GameStats.cs
+++ Unity/Assets/Scripts/GameStats.cs
@@ -6,11 +6,14 @@ public class GameStats : MonoBehaviour
     float SmoothFPS = 0;
     public float Smoothness = 0.99f;
     UILabel Label;
+    float StartTime;
 
     //============================================================================================================================================//
     void Awake()
     {
         Label = GetComponent<UILabel>();
+
+        //StartTime = 
     }
 
     //============================================================================================================================================//
@@ -18,13 +21,16 @@ public class GameStats : MonoBehaviour
     {
         if (!Game.Instance.Paused)
         {
+            // Session Play Time //
+
+
             float fps = 1f / Time.deltaTime;
             SmoothFPS = SmoothFPS * Smoothness + fps * (1f - Smoothness);
             if (float.IsInfinity(SmoothFPS))
             {
                 SmoothFPS = fps;
             }
-            Label.text = SmoothFPS.ToString("N0");// +"fps";
+            Label.text = SmoothFPS.ToString("N0") + " : " + Time.timeSinceLevelLoad.ToString("N0");
         }  
 	}
 }
